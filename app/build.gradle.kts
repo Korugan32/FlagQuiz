c7cad12ccbaa8db339e2588a8ed4a7e9305b7a86
Korugan32 <tahacansenel@gmail.com>
2024-05-21 15:39:28 +0300

    Add Material3 Library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,5 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation("io.coil-kt:coil-compose:2.6.0")
+    implementation("androidx.compose.material3:material3:1.2.1")
 }
