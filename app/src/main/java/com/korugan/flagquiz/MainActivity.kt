67fc5150c86b7c63137190057663fc2a21b948ab
Korugan32 <tahacansenel@gmail.com>
2024-05-21 18:29:13 +0300

    Add And Update

--- app/src/main/java/com/korugan/flagquiz/MainActivity.kt
+++ app/src/main/java/com/korugan/flagquiz/MainActivity.kt
@@ -19,29 +19,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FlagQuizTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FlagQuizTheme {
-        Greeting("Android")
-    }
-}
