a1aa035889b21cfc858d47194e8ee7ae35c9f095
Korugan32 <tahacansenel@gmail.com>
2024-05-21 12:21:50 +0300

    Add Coil Library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("io.coil-kt:coil-compose:2.6.0")
 }
